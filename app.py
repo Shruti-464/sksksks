d696ecf92ef416cfa52d0c96537b4b6010d03989
shruti-464 <kundu.shruti462004@gmail.com>
2025-02-17 13:00:28 +0530

    new project code

--- app.py
+++ app.py
@@ -1,21 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-# Initialize Flask app
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hospital.db'
 db = SQLAlchemy(app)
 
-# Define a simple route
-@app.route('/')
-def home():
-    return "Welcome to the Hospital Management System"
-
-# Before first request, create the tables
-@app.before_first_request
-def create_tables():
+# Manually create tables before running the app
+if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Creates the database tables if they don't exist
-
-if __name__ == '__main__':
     app.run(debug=True)
